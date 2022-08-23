845748c303f1fe133dbc973c02a5c767498ca0dd
perviz023 <90148295+perviz023@users.noreply.github.com>
2022-08-23 20:22:45 +0400

    Add files via upload

---
+++ src/src/Components/Login.tsx
@@ -0,0 +1,76 @@
+import { TextField, Typography, Button } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+
+const Login = () => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event: any) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const submitForm = (event: any) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        <Box
+          display="flex"
+          flexDirection="column"
+          maxWidth={400}
+          alignItems="center"
+          justifyContent="center"
+          margin={"auto"}
+          marginTop={10}
+          padding={5}
+          borderRadius={5}
+          boxShadow={"5px 5px 5px 5px  #ccc"}
+          sx={{
+            ":hover": {
+              boxShadow: "10px 10px 10px 10px #ccc",
+            },
+          }}
+        >
+          <Typography variant="h2" padding={3} textAlign="center">
+            Login
+          </Typography>
+          <TextField
+            name="email"
+            value={inputs.email}
+            onChange={handleChange}
+            margin="normal"
+            type={"email"}
+            variant="outlined"
+            placeholder="E-mail"
+          />
+          <TextField
+            name="password"
+            value={inputs.password}
+            onChange={handleChange}
+            margin="normal"
+            type={"password"}
+            variant="outlined"
+            placeholder="Password"
+          />
+          <Button
+            type="submit"
+            sx={{ marginTop: 3, borderRadius: 3 }}
+            variant="contained"
+          >
+            Login
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
